6619bf541e4e2381a3feaa89ddc074da6932bf40
root <unix.ma@gmail.com>
2018-06-23 04:39:40 +0800

    appending...

--- a.sh
+++
@@ -1,9 +0,0 @@
-while true ; do 
-    date >> time.log
-    cp /home/test/photos/*.jpg .
-    rm -f /home/test/photos/*.jpg
-    git add .
-    git commit -m 'appending...'
-    git push -u origin master
-    sleep 600
-done
